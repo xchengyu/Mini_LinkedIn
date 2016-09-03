6056b3903731d3a6e533316543a10f912dcd9234
xchengyu <xchengyu@usc.edu>
2016-09-02 23:17:00 -0700

    encode decode image

---
+++ app/src/main/java/com/example/chengyu/mini_linkedin/util/PermissionUtils.java
@@ -0,0 +1,38 @@
+package com.example.chengyu.mini_linkedin.util;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+
+/**
+ * Created by Chengyu on 8/24/2016.
+ * Don't know how to design this class, copy from teacher's template
+ */
+public class PermissionUtils {
+
+    public static final int REQ_CODE_WRITE_EXTERNAL_STORAGE = 200;
+
+    public static boolean checkPermission(@NonNull Context context,
+                                          @NonNull String permission) {
+        return Build.VERSION.SDK_INT < Build.VERSION_CODES.M
+                || ContextCompat.checkSelfPermission(context, permission) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    public static void requestPermissions(@NonNull Activity activity,
+                                          @NonNull String[] permissions,
+                                          int reqCode) {
+        ActivityCompat.requestPermissions(activity, permissions, reqCode);
+    }
+
+    public static void requestReadExternalStoragePermission(@NonNull Activity activity) {
+        requestPermissions(activity,
+                new String[] {Manifest.permission.READ_EXTERNAL_STORAGE},
+                REQ_CODE_WRITE_EXTERNAL_STORAGE);
+    }
+
+}
